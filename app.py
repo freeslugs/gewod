ed22c31544b24333eacda0a1b92c8da48e1f9a66
mimouncadosch <mimouncadosch@gmail.com>
2017-04-08 17:43:26 -0500

    Added basic tracking of image

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/")
 def index():
-  return app.send_static_file('index.html')
+  return render_template('index.html')
 
+@app.route("/test")
+def test():
+	return render_template('cv.html')
+	
 if __name__ == "__main__":
   app.run()
